f2e1f4760d515ff7de03690d4c7df6542611e667
mesciuS <giovadev@gmail.com>
2023-03-01 21:08:29 +0100

    feat: lista spesa

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let lista = ["latte", "uova", "patate", "carne", "amarone della valpolicella"];
+
+let contatore = 0;
+
+while(contatore < lista.length) {
+
+    console.log(lista)
+    contatore++;
+}
